ef9ddc779e7d861cb9d59b1064ad67c4d8903bd5
JonKandidiGomez <jonkandidi.gomez@ikasle.egibide.org>
2025-01-15 20:50:05 +0100

    Proyecto Creado
    Proyecto Creado

---
+++ Biblioteca/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Biblioteca")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Biblioteca")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("2b83963e-2e48-4f40-a767-a357348db043")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
